0cbd5aa050c09a7e1fe4ff7ed25ca891185ee409
ArturOle <artur.oleksinski99@gmail.com>
2022-11-12 11:36:53 +0100

    Point and Plot

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
 import dearpygui.dearpygui as dpg
 
-# Placeholder data for plots
-xVal = [i for i in range(10)]
-yVal = [i*i for i in range(10)]
+class Point:
+    def __init__(self, pos, val):
+        self.position = pos
+        self.value = val
+
+    @staticmethod
+    def is_dragable():
+        return True
+
+class PlotData:
+    def __init__(self, x_values, y_values):
+        self.x_axis = x_values
+        self.y_axis = y_values
+
+
+plot_data = PlotData(
+    [i for i in range(10)],
+    [i*i for i in range(10)]
+)
 data = [[150, True], [280, True], [470, True], [600, False], [750, False]]
 
-# Draggable point variables
 point = None
 pointPos = [50, 500]
-
-dpg.create_context()
+points = [Point(pointPos, True)]
 
 with dpg.window(tag="Primary Window"):
-    dpg.add_file_dialog(directory_selector=True, show=False, tag="file_dialog_id")
-    dpg.add_button(label="Select File", callback=lambda: dpg.show_item("file_dialog_id"), height=50, width=200)
+    dpg.add_file_dialog(
+        directory_selector=True,
+        show=False, 
+        tag="file_dialog_id"
+    )
+    dpg.add_button(
+        label="Select File",
+        callback=lambda: dpg.show_item("file_dialog_id"),
+        height=50,
+        width=200
+    )
 
     dpg.add_spacer(height=20)
     
@@ -22,13 +45,13 @@ with dpg.window(tag="Primary Window"):
             dpg.add_plot_legend()
             dpg.add_plot_axis(dpg.mvXAxis, label="x")
             dpg.add_plot_axis(dpg.mvYAxis, label="y", tag="y_axis")
-            dpg.add_line_series(xVal, yVal, label='Data', parent='y_axis')
+            dpg.add_line_series(plot_data.x_axis, plot_data.y_axis, label='Data', parent='y_axis')
 
         with dpg.plot(width = 400):
             dpg.add_plot_legend()
             dpg.add_plot_axis(dpg.mvXAxis, label="x")
             dpg.add_plot_axis(dpg.mvYAxis, label="y", tag="y_axis2")
-            dpg.add_line_series(xVal, yVal, label='Data2', parent='y_axis2')
+            dpg.add_line_series(plot_data.x_axis, plot_data.y_axis, label='Data2', parent='y_axis2')
 
     # Custom 1D graph
     dpg.draw_line([50, 500], [800, 500], color=[200, 200, 200], thickness=2)
@@ -37,9 +60,19 @@ with dpg.window(tag="Primary Window"):
 
     for point in data:
         if point[1] == True:
-            dpg.draw_circle([point[0], 500], radius=10, color=(0, 255, 0), fill=[0, 255, 0])
+            dpg.draw_circle(
+                [point[0], 500],
+                radius=10,
+                color=(0, 255, 0),
+                fill=[0, 255, 0]
+            )
         else:
-            dpg.draw_circle([point[0], 500], radius=10, color=(255, 0, 0), fill=[255, 0, 0])
+            dpg.draw_circle(
+                [point[0], 500],
+                radius=10,
+                color=(255, 0, 0),
+                fill=[255, 0, 0]
+            )
 
     dpg.add_text("F measure etc...", indent=1, pos=[50, 600])
 
@@ -56,7 +89,7 @@ dpg.set_primary_window("Primary Window", True)
 while dpg.is_dearpygui_running():
     if dpg.is_mouse_button_down(0) and abs(dpg.get_mouse_pos()[0] - pointPos[0]) < 50 and abs(dpg.get_mouse_pos()[1] - pointPos[1]) < 50:
         pointPos[0] = dpg.get_mouse_pos()[0]
-        dpg.configure_item(item = point, center=pointPos)
+        dpg.configure_item(item=point, center=pointPos)
 
     dpg.render_dearpygui_frame()
 
