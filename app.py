74c9d0b3d97eb2777354f084fabdd52438c27b6a
ArturOle <artur.oleksinski99@gmail.com>
2022-11-14 10:47:30 +0100

    Optimizing calcualtions

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ class Dragable:
     def is_dragable():
         return True
 
+
 class Point(Dragable):
     y_pos = 500
     radius = 10
     _green = (0, 255, 0)
     _red = (255, 0, 0)
+
     def __init__(self, pos, val):
         self.x_pos = pos
         self.value = val
@@ -40,11 +42,13 @@ class Point(Dragable):
             fill=self._green
         )
 
+
 class Threshold(Dragable):
     y_pos = 500
     half_length = 10
     thickness = 4
     color = [230, 230, 230]
+
     def __init__(self, x_pos):
         self.x_pos = x_pos
 
@@ -53,7 +57,7 @@ class Threshold(Dragable):
             [self.x_pos, self.y_pos-self.half_length],
             [self.x_pos, self.y_pos+self.half_length],
             color=self.color,
-            thickness = self.thickness
+            thickness=self.thickness
         )
 
 
@@ -114,16 +118,17 @@ class PlotData:
         self.x_axis = x_values
         self.y_axis = y_values
 
+
 def generate_example_points(
         x_range: tuple,
         nr_of_points: int = 10,
         true_or_false_prc: float = 0.5
-    ):
+        ):
     return [
         Point(
-            randint(*x_range), 
+            randint(*x_range),
             choices(
-                [False, True], 
+                [False, True],
                 [1-true_or_false_prc, true_or_false_prc]
             )[0]
         ) for i in range(nr_of_points)
@@ -144,14 +149,6 @@ def bounds_check(point, max_distance=20):
 
     return False
 
-# Placeholder data for plots
-xVal = [i for i in range(10)]
-yVal = [i*i for i in range(10)]
-data = [[150, True], [280, True], [470, True], [600, False], [700, False]]
-
-# Draggable point variables
-point = None
-pointPos = [50, 500]
 
 dpg.create_context()
 plot_data = PlotData(
@@ -163,48 +160,18 @@ axis_data = {
     "threshold": Threshold(400)
 }
 
-def find_circle(px: int):
-    for i in range(len(data)):
-        if abs(data[i][0] - px) < 10:
-            return i
-
-    return -1
-
-def mouse_down():
-    dpg.set_value("is_mouse_down", True)
-    current_circle = dpg.get_value("current_circle")
-    if current_circle == -1:
-        px, py = dpg.get_mouse_pos(local=False)
-        if py > 480 and py < 520:
-            index = find_circle(px)
-            if index != -1:
-                dpg.set_value("current_circle", index)
-
-def mouse_release():
-    dpg.set_value("is_mouse_down", False)
-    dpg.set_value("current_circle", -1)
-
-
-def move_circle():
-    px, py = dpg.get_mouse_pos(local=False)
-    current_circle = dpg.get_value("current_circle")
-    if current_circle != -1:
-        indexStr = str(current_circle)
-        dpg.configure_item(f"circle_{indexStr}", center=[px, 500])
-        data[current_circle][0] = px
-
-with dpg.value_registry():
-    dpg.add_bool_value(default_value=False, tag="is_mouse_down")
-    dpg.add_int_value(default_value=-1, tag="current_circle")
-
-with dpg.handler_registry():
-    dpg.add_mouse_move_handler(callback=move_circle)
-    dpg.add_mouse_down_handler(callback=mouse_down)
-    dpg.add_mouse_release_handler(callback=mouse_release)
-
-with dpg.window(tag="Primary Window",width=800, height=600):
-    dpg.add_file_dialog(directory_selector=True, show=False, tag="file_dialog_id")
-    dpg.add_button(label="Select File", callback=lambda: dpg.show_item("file_dialog_id"), height=50, width=200)
+with dpg.window(tag="Primary Window"):
+    dpg.add_file_dialog(
+        directory_selector=True,
+        show=False,
+        tag="file_dialog_id"
+    )
+    dpg.add_button(
+        label="Select File",
+        callback=lambda: dpg.show_item("file_dialog_id"),
+        height=50,
+        width=200
+    )
 
     dpg.add_spacer(height=20)
 
@@ -244,15 +211,19 @@ with dpg.window(tag="Primary Window",width=800, height=600):
     dpg.draw_line([50, 500], [750, 500], color=[200, 200, 200], thickness=2)
 
     # Threshold
-    data["threshold"].draw()
+    axis_data["threshold"].draw()
 
     # Drawing points
-    for point in data["points"]:
+    for point in axis_data["points"]:
         point.draw()
 
     dpg.add_text("F measure etc...", indent=1, pos=[50, 600])
 
-dpg.create_viewport(title='Classification Metrics Demonstrator', width=850, resizable=False)
+dpg.create_viewport(
+    title='Classification Metrics Demonstrator',
+    width=850,
+    resizable=False
+)
 dpg.setup_dearpygui()
 
 # dpg.show_style_editor()
