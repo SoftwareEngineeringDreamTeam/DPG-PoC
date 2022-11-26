0b468479c902ca3bc810f3c617834a01d72912a1
ArturOle <artur.oleksinski99@gmail.com>
2022-11-26 12:14:14 +0100

    Axis class

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,18 @@
 from random import choices, randint
 
-from axis import Point
-
 
 def generate_example_points(
         x_range: tuple,
+        point_class,
         nr_of_points: int = 10,
         true_or_false_prc: float = 0.5
-        ):
+):
     return [
-        Point(
+        point_class(
             randint(*x_range),
             choices(
                 [False, True],
                 [1-true_or_false_prc, true_or_false_prc]
             )[0]
-        ) for i in range(nr_of_points)
+        ) for _ in range(nr_of_points)
     ]
