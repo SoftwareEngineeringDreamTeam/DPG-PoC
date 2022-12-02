308e1fff75c61d0b5c55d58481837f60aef2ac0a
amrukwa <a.mrukwa00@gmail.com>
2022-12-02 20:18:05 +0100

    add pylint ignores

--- src/plot.py
+++ src/plot.py
@@ -1,3 +1,9 @@
+# pylint: disable=missing-docstring
+# pylint: disable=too-few-public-methods
+# pylint: disable=unused-import
+# pylint: disable=import-error
+# pylint: disable=W
+
 import plotly.graph_objects as graph_obj
 
 from __init__ import dpg
