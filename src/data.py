f65a062cf42d931f945ba0572a415f72c81741fe
amrukwa <a.mrukwa00@gmail.com>
2022-12-04 00:25:22 +0100

    add precision to data

--- src/data.py
+++ src/data.py
@@ -17,13 +17,13 @@ class Data:
     def _update_accuracy_score(self):
         pass
 
+    def _update_precision_score(self):
+        pass
+
     def _update_recall_score(self):
         pass
 
     def _update_roc_curve(self):
-        pass
-
-    def _update_auc_curve(self):
         pass
 
     def _update_mmc_score(self):
---
+++ src/metrics.py
@@ -0,0 +1,39 @@
+class Metrics:
+    def _get_true_pos(self, y_true, y_pred):
+        pass
+
+    def _get_true_neg(self, y_true, y_pred):
+        pass
+
+    def _get_false_pos(self, y_true, y_pred):
+        pass
+
+    def _get_false_neg(self, y_true, y_pred):
+        pass
+
+    def convert_to_binary(self, y, thr):
+        pass
+
+    def calculate_precision(self, y_true, y_pred):
+        pass
+
+    def calculate_recall(self, y_true, y_pred):
+        pass
+
+    def calculate_f1_score(self, y_true, y_pred):
+        pass
+
+    def calculate_accuracy(self, y_true, y_pred):
+        pass
+
+    def calculate_tp_rate(self, y_true, y_pred):
+        pass
+
+    def calculate_fp_rate(self, y_true, y_pred):
+        pass
+
+    def calculate_mmc(self, y_true, y_pred):
+        pass
+
+    def calculate_confusion_matrix(self, y_true, y_pred):
+        pass
