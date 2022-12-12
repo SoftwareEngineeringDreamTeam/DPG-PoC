9cea24e6b1a223b23e3cfbbf717cba00f9cb3457
ArturOle <artur.oleksinski99@gmail.com>
2022-12-12 21:01:47 +0100

    bump up setup version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name="ROC'n'ROLL",
     packages=['src'],
     author='SoftwareEngineeringDreamTeam',
-    version='0.16.0',
+    version='0.17.1',
     install_requires=[
         'numpy',
         'dearpygui',
